b339404a2299bd94c0e44d9e38c84a3d781ef635
Swoonks <82228740+Swoonks@users.noreply.github.com>
2021-05-13 09:50:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const activities_list = [
+    "Wait something.", 
+    "Help this server",
+    "Eat a cookie", 
+    "Try to get out"
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
+    
+    client.on('ready', () => {
+        setInterval(() => {
+            const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+            client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+        }, 10000); // Runs this every 10 seconds.
+    });
